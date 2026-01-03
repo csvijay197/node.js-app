920380f2931ca9159d260347f5824681ffcdfcfc
csvijay197 <csvijay197>
2026-01-04 00:53:50 +0530

    Initial node.js with docker and k8s

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	  res.send('Hello from Jenkins → Docker → Kubernetes ');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+	  console.log('App running on port ${PORT}');
+});
